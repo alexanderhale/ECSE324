f7870ec3847bdbd8a834a9ced4fa9aa0a485ac08
halestorm962 <a.hale@live.ca>
2017-10-19 17:36:43 -0400

    unknown changes

--- Lab 3/G12_Lab3/drivers/asm/LEDs.s
+++ Lab 3/G12_Lab3/drivers/asm/LEDs.s
@@ -13,10 +13,12 @@ read_LEDs_ASM:
 	BX LR				// leave
 
 write_LEDs_ASM:
-	PUSH {R1, LR}
+	PUSH {R1}
+	PUSH {LR}
 	LDR R1, =SW_BASE	// load the memory address where we'll put the value
 	STR R0, [R1]		// store value of R0 to memory address in R1
-	POP {LR, R1}
+	POP {LR}
+	POP {R1}
 	BX LR
 
 	.end
