9d2367b0cc8c270e4de2cb22b90b4dff3b8c625c
halestorm962 <a.hale@live.ca>
2017-10-19 14:50:13 -0400

    part 1 in progress

---
+++ Lab 3/G12_Lab3/drivers/asm/LEDs.s
@@ -0,0 +1,22 @@
+	.text
+	.equ SW_BASE, 0xFF200040
+	.global read_LEDs_ASM
+	.global write_LEDs_ASM
+
+read_LEDs_ASM:
+	PUSH {R1}
+	PUSH {LR}
+	LDR R1, =SW_BASE	// load the memory address from which we'll get the value
+	LDR R0, [R1]		// get the value and put it into R0
+	POP {LR}
+	POP {R1}
+	BX LR				// leave
+
+write_LEDs_ASM:
+	PUSH {R1, LR}
+	LDR R1, =SW_BASE	// load the memory address where we'll put the value
+	STR R0, [R1]		// store value of R0 to memory address in R1
+	POP {LR, R1}
+	BX LR
+
+	.end
---
+++ Lab 3/G12_Lab3/drivers/asm/slider_switches.s
@@ -0,0 +1,10 @@
+	.text
+	.equ SW_BASE, 0xFF200040
+	.global read_slider_switches_ASM
+	
+read_slider_switches_ASM:
+	LDR R1, =SW_BASE
+	LDR R0, [R1]
+	BX LR
+
+	.end	
