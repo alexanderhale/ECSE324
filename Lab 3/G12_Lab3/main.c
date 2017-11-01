fb59e85756fbdea38d1987f99f9cbb8bc770463a
halestorm962 <a.hale@live.ca>
2017-11-01 16:29:02 -0400

    started part 2 and part 3

--- Lab 3/G12_Lab3/main.c
+++ Lab 3/G12_Lab3/main.c
@@ -4,8 +4,13 @@
 #include "./drivers/inc/slider_switches.h"
 #include "./drivers/inc/HEX_displays.h"
 #include "./drivers/inc/pushbuttons.h"
+#include "./drivers/inc/HPS_TIM.h"
+#include "./drivers/inc/int_setup.h"
+#include "./drivers/inc/ISRs.h"
 
 int main() {
+	/*	------------	PART 1 - Basic I/O Demonstration   --------------- */
+
 	// perform continuously
 	while (1) {	
 		// illuminate LED for each switch
@@ -16,7 +21,7 @@ int main() {
 		int keys = 0xF & read_PB_data_ASM();			// only keep first four digits of binary representation	
 
 		// illuminate the appropriate hex displays with the correct number
-		if ((keys > 0) && ((0x200 & read_slider_switches_ASM()) != 512) {
+		if ((keys > 0) && ((0x200 & read_slider_switches_ASM()) != 512)) {
 			// if any pushbuttons are pressed, flood HEX4 and HEX5
 			HEX_flood_ASM(HEX4 | HEX5);
 			
@@ -58,5 +63,99 @@ int main() {
 			HEX_clear_ASM(HEX0 | HEX1 | HEX2 | HEX3 | HEX4 | HEX5);
 		}
 	}
+
+
+	/*	------------	PART 2 - Timer-based Stopwatch   --------------- 
+
+	int count0 = 0, count1 = 0, count2 = 0, count3 = 0;
+
+	HPS_TIM_config_t hps_tim;
+
+	hps_tim.tim = TIM0|TIM1|TIM2|TIM3;
+	hps_tim.timeout = 1000000;
+	hps_tim.LD_en = 1;
+	hps_tim.INT_en = 1;
+	hps_tim.enable = 1;
+
+	HPS_TIM_config_ASM(&hps_tim);
+
+	while(1) {
+		if (HPS_TIM_read_INT_ASM(TIM0)) {
+			
+			HPS_TIM_clear_INT_ASM(TIM0);
+
+			if (++count0 == 16) {
+				count0 = 0;
+			}
+
+			HEX_write_ASM(HEX0, count0);
+		}
+
+		if (HPS_TIM_read_INT_ASM(TIM1)) {
+			
+			HPS_TIM_clear_INT_ASM(TIM1);
+
+			if (++count1 == 16) {
+				count1 = 0;
+			}
+
+			HEX_write_ASM(HEX1, count1);
+		}
+
+		if (HPS_TIM_read_INT_ASM(TIM2)) {
+			
+			HPS_TIM_clear_INT_ASM(TIM2);
+
+			if (++count2 == 16) {
+				count2 = 0;
+			}
+
+			HEX_write_ASM(HEX2, count2);
+		}
+
+		if (HPS_TIM_read_INT_ASM(TIM3)) {
+			
+			HPS_TIM_clear_INT_ASM(TIM3);
+
+			if (++count3 == 16) {
+				count3 = 0;
+			}
+
+			HEX_write_ASM(HEX3, count3);
+		}
+	}*/
+
+
+	/*	------------	PART 3 - INTERRUPTS EXAMPLE   --------------- 
+
+	int_setup(1, (int []){199});
+
+	int count = 0;
+	HPS_TIM_config_t hps_tim;
+
+	hps_tim.tim = TIM0;
+	hps_tim.timeout = 1000000;
+	hps_tim.LD_en = 1;
+	hps_tim.INT_en = 1;
+	hps_tim.enable = 1;
+
+	HPS_TIM_config_ASM(&hps_tim);
+
+	while(1) {
+
+		if (hps_tim0_int_flag) {
+			hps_tim0_int_flag = 0;
+
+			if (+count == 15) {
+				count = 0;
+			}
+
+			HEX_write_ASM(HEX0, count);
+		}
+	}*/
+
 	return 0;
+
+
+	/*	------------	PART 3 - Interrupt-based Stopwatch  --------------- */
 }
